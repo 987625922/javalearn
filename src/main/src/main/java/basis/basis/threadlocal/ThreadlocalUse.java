aae75c7302b084e8f682e2da1c413dd8e403471a
wind <987625922@qq.com>
2020-05-09 09:57:50 +0800

    添加二叉树和平衡二叉树的代码

--- src/main/src/main/java/basis/basis/threadlocal/ThreadlocalUse.java
+++ src/main/src/main/java/basis/basis/threadlocal/ThreadlocalUse.java
@@ -54,8 +54,10 @@ public class ThreadlocalUse {
          * 如果下面的值不需要在线程中共享，就可以使用ThreadLocal让每个线程内部都会有一个该变量，且在线程内部任何地方都可以使用，
          * 线程之间互不影响，这样一来就不存在线程安全问题，也不会严重影响程序执行性能。
          * 但是由于在每个线程中都创建了副本，所以要考虑它对资源的消耗，比如内存的占用会比不使用ThreadLocal要大。
-         * 原理使用了Map的方法，在每个线程Thread内部有一个ThreadLocal.ThreadLocalMap类型的成员变量threadLocals，这个threadLocals就是用来存储实际的变量副本的，键值为当前ThreadLocal变量，value为变量副本（即T类型的变量）。
-         * 初始时，在Thread里面，threadLocals为空，当通过ThreadLocal变量调用get()方法或者set()方法，就会对Thread类中的threadLocals进行初始化，并且以当前ThreadLocal变量为键值，以ThreadLocal要保存的副本变量为value，存到threadLocals。
+         * 原理使用了Map的方法，在每个线程Thread内部有一个ThreadLocal.ThreadLocalMap类型的成员变量threadLocals，
+         * 这个threadLocals就是用来存储实际的变量副本的，键值为当前ThreadLocal变量，value为变量副本（即T类型的变量）。
+         * 初始时，在Thread里面，threadLocals为空，当通过ThreadLocal变量调用get()方法或者set()方法，
+         * 就会对Thread类中的threadLocals进行初始化，并且以当前ThreadLocal变量为键值，以ThreadLocal要保存的副本变量为value，存到threadLocals。
          * 然后在当前线程里面，如果要使用副本变量，就可以通过get方法在threadLocals里面查找。
          */
         private static ThreadLocal<StringBuilder> counter = new ThreadLocal<StringBuilder>() {
