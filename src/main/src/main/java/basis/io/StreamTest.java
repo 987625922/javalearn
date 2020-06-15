f4cc3811fa0f4f21eb7cb7eb7f2e4686397bc78f
wind <987625922@qq.com>
2020-06-15 18:29:30 +0800

    io学习

--- src/main/src/main/java/basis/io/StreamTest.java
+++ src/main/src/main/java/basis/io/StreamTest.java
@@ -1,8 +1,10 @@
 package basis.io;
 
+import org.junit.Test;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -17,20 +19,30 @@ import java.io.IOException;
  */
 public class StreamTest {
 
-    public static void main(String[] args) {
-        File file = new File("d://log.txt");
-        try {
-            FileInputStream fis = new FileInputStream(file);
-            byte[] all = new byte[(int) file.length()];
-            fis.read(all);
-            for (byte b : all) {
-                System.out.println(b);
-            }
-            System.out.println(all.length);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void test() throws IOException {
+        outPutStreamTest();
+        inputStreamTest();
     }
+
+    //已字节流读取文件
+    private void inputStreamTest() throws IOException {
+        File file = new File("d://log.txt");
+        FileInputStream fis = new FileInputStream(file);
+        byte[] all = new byte[(int) file.length()];
+        fis.read(all);
+        for (byte b : all) {
+            System.out.println(b);
+        }
+        fis.close();
+    }
+
+    private void outPutStreamTest() throws IOException {
+        File file = new File("d://log.txt");
+        byte data[] = {88, 89};
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(data);
+        fos.close();
+    }
+
 }
